3929060727a537172f5aa9bdb017e9378667c417
Vlad <yellow31031995@gmail.com>
2019-10-02 23:09:46 +0300

    Story 84: added to insert data a table dept b

---
+++ Homework_5/SQL/Additional tasks/Data population/to_insert_a_table_dept_b.sql
@@ -0,0 +1,3 @@
+INSERT INTO `additional_task`.`dept b` (`DEPTID`, `DEPTNAME`) VALUES ('10', 'QA');
+INSERT INTO `additional_task`.`dept b` (`DEPTID`, `DEPTNAME`) VALUES ('20', 'DEV');
+INSERT INTO `additional_task`.`dept b` (`DEPTID`, `DEPTNAME`) VALUES ('30', 'IT');
