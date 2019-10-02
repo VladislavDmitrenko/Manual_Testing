c0ba45545ad151d40a10bae0f415010454944acf
Vlad <yellow31031995@gmail.com>
2019-10-02 14:14:43 +0300

    Story 37: changed a table customer

--- Homework_5/SQL/Advanced tasks/1/Table creation/created_a_table_Customers.sql
+++ Homework_5/SQL/Advanced tasks/1/Table creation/created_a_table_Customers.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `advanced_task1`.`Сustomers` (
-  `CustomerID` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE `advanced_task1`.`customers` (
+  `CustomerID` INT NOT NULL,
   `Name` VARCHAR(50) NULL,
   `Age` INT NULL,
   `CountryID` INT NULL,
