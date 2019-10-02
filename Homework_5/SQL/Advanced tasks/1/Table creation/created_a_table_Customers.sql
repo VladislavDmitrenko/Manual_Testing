d7e7d5f7011b9f9e45809d0d595fc708bb351040
Vlad <yellow31031995@gmail.com>
2019-10-02 13:27:23 +0300

    Story 34: created the folder advanced tasks and created a database and a table customers

---
+++ Homework_5/SQL/Advanced tasks/1/Table creation/created_a_table_Customers.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `advanced_task1`.`Сustomers` (
+  `CustomerID` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(50) NULL,
+  `Age` INT NULL,
+  `CountryID` INT NULL,
+  PRIMARY KEY (`CustomerID`));
