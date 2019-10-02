e19c0e814878f83ff8997601ca90c557670749d7
Vlad <yellow31031995@gmail.com>
2019-10-02 19:52:45 +0300

    Story 59: tp insert data to table rating

---
+++ Homework_5/SQL/Advanced tasks/2/Data population/to_insert_data_to_table_rating.sql
@@ -0,0 +1,9 @@
+INSERT INTO `advanced_task2`.`rating` (`RatingId`, `ReportingContextId`, `RatingAgencyID`, `RatingValue`) VALUES ('11101', '111', '101', '(P)Ba2');
+INSERT INTO `advanced_task2`.`rating` (`RatingId`, `ReportingContextId`, `RatingAgencyID`, `RatingValue`) VALUES ('11201', '112', '102', 'Baa1');
+INSERT INTO `advanced_task2`.`rating` (`RatingId`, `ReportingContextId`, `RatingAgencyID`, `RatingValue`) VALUES ('11301', '113', '103', 'Baa2');
+INSERT INTO `advanced_task2`.`rating` (`RatingId`, `ReportingContextId`, `RatingAgencyID`, `RatingValue`) VALUES ('11401', '114', '104', '(P)B2');
+INSERT INTO `advanced_task2`.`rating` (`RatingId`, `ReportingContextId`, `RatingAgencyID`, `RatingValue`) VALUES ('11501', '115', '105', '(P)B3');
+INSERT INTO `advanced_task2`.`rating` (`RatingId`, `ReportingContextId`, `RatingAgencyID`, `RatingValue`) VALUES ('11601', '116', '106', 'AA-');
+INSERT INTO `advanced_task2`.`rating` (`RatingId`, `ReportingContextId`, `RatingAgencyID`, `RatingValue`) VALUES ('11701', '117', '107', 'B+ f');
+INSERT INTO `advanced_task2`.`rating` (`RatingId`, `ReportingContextId`, `RatingAgencyID`, `RatingValue`) VALUES ('11801', '118', '108', 'BB f');
+INSERT INTO `advanced_task2`.`rating` (`RatingId`, `ReportingContextId`, `RatingAgencyID`, `RatingValue`) VALUES ('11901', '119', '109', 'BBB- f');
