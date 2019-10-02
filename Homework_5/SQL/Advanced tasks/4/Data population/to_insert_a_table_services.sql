9a4dd5737d9e6f29e91e7354dfa6b6f527aae880
Vlad <yellow31031995@gmail.com>
2019-10-02 22:21:15 +0300

    Story 78:  created to folder data polution and added to insert a table services

---
+++ Homework_5/SQL/Advanced tasks/4/Data population/to_insert_a_table_services.sql
@@ -0,0 +1,5 @@
+INSERT INTO `advanced_task4`.`services` (`Id`, `ClientId`, `SergiceId`, `Date`, `RegionId`) VALUES ('1', '1233', '1', '2013-01-01', '4');
+INSERT INTO `advanced_task4`.`services` (`Id`, `ClientId`, `SergiceId`, `Date`, `RegionId`) VALUES ('2', '1233', '2', '2013-01-05', '4');
+INSERT INTO `advanced_task4`.`services` (`Id`, `ClientId`, `SergiceId`, `Date`, `RegionId`) VALUES ('3', '2789', '1', '2013-01-22', '3');
+INSERT INTO `advanced_task4`.`services` (`Id`, `ClientId`, `SergiceId`, `Date`, `RegionId`) VALUES ('4', '2789', '2', '2013-02-17', '7');
+INSERT INTO `advanced_task4`.`services` (`Id`, `ClientId`, `SergiceId`, `Date`, `RegionId`) VALUES ('5', '3321', '4', '2012-12-05', '8');
