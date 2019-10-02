2a778ee521ce4771576ab6f4a4d96776b335daae
Vlad <yellow31031995@gmail.com>
2019-10-02 22:18:41 +0300

    Story 77: created to folder advanced task4 and new database, created to folder table creation and added a table services

---
+++ Homework_5/SQL/Advanced tasks/4/Table creation/created_to_table_services.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `advanced_task4`.`services` (
+  `Id` INT NOT NULL,
+  `ClientId` INT NULL,
+  `SergiceId` INT NULL,
+  `Date` DATE NULL,
+  `RegionId` INT NULL,
+  PRIMARY KEY (`Id`));
