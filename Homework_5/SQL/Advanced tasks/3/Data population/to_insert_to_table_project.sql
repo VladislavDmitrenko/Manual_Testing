81080bc27658b19f3bcea4f4782acc87c0d4cf89
Vlad <yellow31031995@gmail.com>
2019-10-02 21:17:28 +0300

    Story 68: created to folder data polution and added to insert a table project

---
+++ Homework_5/SQL/Advanced tasks/3/Data population/to_insert_to_table_project.sql
@@ -0,0 +1,9 @@
+INSERT INTO `advanced_task3`.`project` (`p_id`, `name`) VALUES ('1', 'computrer');
+INSERT INTO `advanced_task3`.`project` (`p_id`, `name`) VALUES ('2', 'mobile phone');
+INSERT INTO `advanced_task3`.`project` (`p_id`, `name`) VALUES ('3', 'television');
+INSERT INTO `advanced_task3`.`project` (`p_id`, `name`) VALUES ('4', 'car');
+INSERT INTO `advanced_task3`.`project` (`p_id`, `name`) VALUES ('5', 'bus');
+INSERT INTO `advanced_task3`.`project` (`p_id`, `name`) VALUES ('6', 'bicycle');
+INSERT INTO `advanced_task3`.`project` (`p_id`, `name`) VALUES ('7', 'house');
+INSERT INTO `advanced_task3`.`project` (`p_id`, `name`) VALUES ('8', 'shop');
+INSERT INTO `advanced_task3`.`project` (`p_id`, `name`) VALUES ('9', 'web-site');
