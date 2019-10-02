0ddd06002b21b92aa84d71b4418548bd2afb55ea
Vlad <yellow31031995@gmail.com>
2019-10-02 14:24:28 +0300

    Story 40: fill a table country in a database

---
+++ Homework_5/SQL/Advanced tasks/1/Data population/Fill_tables_country.sql
@@ -0,0 +1,8 @@
+INSERT INTO `advanced_task1`.`country` (`ID`, `Name`) 
+VALUES ('1', 'Ukraine');
+INSERT INTO `advanced_task1`.`country` (`ID`, `Name`) 
+VALUES ('5', 'USA');
+INSERT INTO `advanced_task1`.`country` (`ID`, `Name`) 
+VALUES ('15', 'Spain');
+INSERT INTO `advanced_task1`.`country` (`ID`, `Name`) 
+VALUES ('37', 'UK');
