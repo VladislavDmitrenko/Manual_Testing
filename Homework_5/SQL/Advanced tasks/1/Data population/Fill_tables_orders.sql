1ddda0ac5bc4adff5f3661f510fad9f5b529453a
Vlad <yellow31031995@gmail.com>
2019-10-02 14:22:18 +0300

    Story 39: fill a table orders in a database

---
+++ Homework_5/SQL/Advanced tasks/1/Data population/Fill_tables_orders.sql
@@ -0,0 +1,8 @@
+INSERT INTO `advanced_task1`.`orders` (`OrderID`, `CustomerID`, `Status`, `Price`) 
+VALUES ('999', '2', 'Complete', '100');
+INSERT INTO `advanced_task1`.`orders` (`OrderID`, `CustomerID`, `Status`, `Price`) 
+VALUES ('1000', '3', 'In progress', '200');
+INSERT INTO `advanced_task1`.`orders` (`OrderID`, `CustomerID`, `Status`, `Price`) 
+VALUES ('1001', '1', 'Open', '50');
+INSERT INTO `advanced_task1`.`orders` (`OrderID`, `CustomerID`, `Status`, `Price`) 
+VALUES ('1002', '4', 'Complete', '300');
