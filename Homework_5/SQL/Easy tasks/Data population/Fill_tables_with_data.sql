6cd61e316f05f9e0efde9518bc9de7e307d60038
Vlad <yellow31031995@gmail.com>
2019-10-02 12:46:42 +0300

    Story 31: Fill tables with data (100 different entries in PeopleInfo, 30 entries in CountryInfo)

---
+++ Homework_5/SQL/Easy tasks/Data population/Fill_tables_with_data.sql
@@ -0,0 +1,424 @@
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('3', 'Allen', 'Ponte', '1', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('4', 'Li', 'Colter', '2', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('5', 'Maiko', 'Sebring', '3', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('6', 'Kaimi', 'Fortson', '4', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('7', 'Essien', 'Saba', '5', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('8', 'Pericles', 'Dolin', '6', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('9', 'Emanuela', 'Darrah', '7', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('10', 'Madura', 'Funke', '8', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('11', 'Manus', 'Shank', '9', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('12', 'Maris', 'Joachim', '10', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('13', 'Sinnett', 'Bancroft', '11', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('14', 'Waleran', 'Boykins', '12', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('15', 'Phoena', 'Maner', '13', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('16', 'Dedrick', 'Relyea', '14', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('17', 'Abhi', 'Casas', '15', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('18', 'Abhilasha', 'Duplantis', '16', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('19', 'Adamina', 'Ogawa', '17', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('20', 'Cecile', 'Slaughter', '18', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('21', 'Roper', 'Starrett', '19', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('22', 'Cardew', 'Arnette', '20', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('23', 'Derrick', 'Gelinas', '21', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('24', 'Standen', 'Terrones', '22', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('25', 'Bharat', 'Myers', '23', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('26', 'Karyn', 'Risner', '24', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('27', 'Rodrigo', 'McLaren', '25', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('28', 'Meta', 'Chee', '26', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('29', 'Geoffrey', 'Silvera', '27', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('30', 'Canice', 'Silvera', '28', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('31', 'Rodrigo', 'Kreutzer', '29', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('32', 'Anoop', 'McLaren', '30', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('33', 'Acedia', 'Maus', '1', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('34', 'Cecile', 'Kunkel', '2', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('35', 'Alec', 'Savard', '3', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('36', 'Roper', 'Risner', '4', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('37', 'Howe', 'Wessel', '5', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('38', 'Kaiya', 'Cress', '6', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('39', 'Gaea', 'Manalo', '7', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('40', 'Oberon', 'Coakley', '8', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('41', 'Brona', 'Garren', '9', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('42', 'Xanthus', 'Reaves', '10', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('43', 'Canice', 'Nowak', '11', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('44', 'Elnora', 'Garman', '12', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('45', 'Maiko', 'Gaughan', '13', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('46', 'Coltin', 'Casas', '14', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('47', 'Sinnett', 'Stager', '15', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('48', 'Alec', 'Early', '16', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('49', 'Tezer', 'Nicastro', '17', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('50', 'Katrin', 'Arispe', '18', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('51', 'Casimir', 'Meza', '1', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('52', 'Yadon', 'Chery', '2', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('53', 'Sakari', 'Proctor', '3', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('54', 'Kane', 'Harrill', '4', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('55', 'Erika', 'Isbell', '5', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('56', 'Kayin', 'Pavia', '6', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('57', 'Comfort', 'Tolleson', '7', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('58', 'Froth', 'Castellanos', '8', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('59', 'Kyon', 'Braswell', '9', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('60', 'Flos', 'Meszaros', '10', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('61', 'Eberta', 'Pence', '11', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('62', 'Pramsu', 'Sandy', '12', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('63', 'Maxime', 'Iverson', '13', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('64', 'Kuhuk', 'Ballentine', '14', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('65', 'Bessie', 'Lentini', '15', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('66', 'Rangi', 'Fager', '16', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('67', 'Erek', 'Kaneshiro', '17', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('68', 'Sachin', 'Angeles', '18', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('69', 'Brishen', 'Siefert', '19', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('70', 'Elgar', 'Oconnor', '20', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('71', 'Sabre', 'Delacruz', '21', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('72', 'Froth', 'Guffey', '22', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('73', 'Erek', 'Ensign', '21', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('74', 'Erek', 'Ensign', '22', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('75', 'Eliot', 'Chery', '23', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('76', 'Kaelyn', 'Rotella', '24', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('77', 'Philena', 'Bolger', '25', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('78', 'Ona', 'Malinowski', '26', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('79', 'Dagna', 'Putney', '27', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('80', 'Nesta', 'Bolger', '28', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('81', 'Barden', 'Norden', '29', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('82', 'Wilfreda', 'Ricciardi', '30', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('83', 'Hillary', 'Gadsden', '1', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('84', 'Pranav', 'Pons', '2', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('85', 'Orelious', 'Ayala', '3', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('86', 'Bari', 'Drinkard', '4', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('87', 'Tanisha', 'Piazza', '5', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('88', 'Simoni', 'Endres', '6', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('89', 'Benedict', 'Purnell', '7', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('90', 'Dagna', 'Thorne', '8', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('91', 'Gilana', 'Friedman', '9', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('92', 'Hemendra', 'Bolger', '10', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('93', 'Polona', 'Filkins', '11', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('94', 'Chevonne', 'Soni', '12', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('95', 'Abhay', 'Jeffreys', '13', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('96', 'Brigham', 'Renken', '14', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('97', 'Radmilla', 'Jeffreys', '15', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('98', 'Jillian', 'Born', '16', '0');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('99', 'Delbert', 'Stocking', '17', '1');
+
+REPLACE INTO `easy_task`.`peopleinfo` 
+(`ID`, `Name`, `Surname`, `CityID`, `isOccupied`) 
+VALUES ('100', 'Polona', 'Putney', '18', '0');
+
+
+
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('United Kingdom', 'London', 8136000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Spain', 'Madrid', 3166000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('South Korea', 'Daegu', 2455000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('China', 'Chaozhou', 2669000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Vietnam', 'Ho Chi Minh City', 8611000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('South Korea', 'Busan', 3400000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Taiwan', 'Taichung', 2792000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('India', 'Kolkata', 4497000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Poland', 'Warsaw', 1735000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('China', 'Foshan', 7197000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Chile', 'Santiago', 5150000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Zimbabwe', 'Harare', 1485000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Japan', 'Kawasaki', 1475000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('China', 'Beijing', 21710000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('China', 'Zunyi', 1095000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Iran', 'Ahvaz', 1112000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Iraq', 'Baghdad', 7665000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('India', 'Bhopal', 1798000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('China', 'Changchun', 7665000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Tanzania', 'Dar es Salaam', 454300, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Turkey', 'Ankara', 5464000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Italy', 'Milan', 1300000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Nigeria', 'Lagos', 2400000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('United States', 'San Diego', 540000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Ghana', 'Accra', 3454000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('China', 'Harbin', 1234000, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Canada', 'Montreal', 675800, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Ukraine', 'Kyiv', 2804000, 1);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Ukraine', 'Truskavets', 29500, 0);
+INSERT INTO `easy_task`.`countryinfo` (`CountryName`, `CityName`, `Population`, `isCapital`) VALUES ('Ukraine', 'Ovruch', 16600, 0);
