33cf03c94be160e934864a0642998751496f005e
Vlad <yellow31031995@gmail.com>
2019-10-02 19:51:22 +0300

    Story 58: changed a table rating

--- Homework_5/SQL/Advanced tasks/2/Table creation/created_a_table_rating.sql
+++ Homework_5/SQL/Advanced tasks/2/Table creation/created_a_table_rating.sql
@@ -1,14 +1,6 @@
 CREATE TABLE `advanced_task2`.`rating` (
   `RatingId` INT NOT NULL,
   `ReportingContextId` INT NOT NULL,
-  `EffectiveStartTimestamp` VARCHAR(50) NULL,
-  `EffectiveEndTimestamp` VARCHAR(50) NULL,
-  `RatingAgencyID` VARCHAR(50) NULL,
-  `RatingTypeId` VARCHAR(50) NULL,
-  `RatingValue` VARCHAR(50) NULL,
-  `Ranking` VARCHAR(50) NULL,
-  `ECAICreditQualityStep` VARCHAR(50) NULL,
-  `IsInferredData` VARCHAR(50) NULL,
-  `ReferenceDataSnapshotId` VARCHAR(50) NULL,
-  `ProcessExecutionId` VARCHAR(50) NULL,
+  `RatingAgencyID` INT NOT NULL,
+  `RatingValue` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`RatingId`, `ReportingContextId`));
