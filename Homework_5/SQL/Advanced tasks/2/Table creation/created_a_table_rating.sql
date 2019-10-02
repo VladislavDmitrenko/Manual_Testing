2a30b20b9c45dfc4c970c96ac5187b013ce6911c
Vlad <yellow31031995@gmail.com>
2019-10-02 15:43:23 +0300

    Story 51: created table rating

---
+++ Homework_5/SQL/Advanced tasks/2/Table creation/created_a_table_rating.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `advanced_task2`.`rating` (
+  `RatingId` INT NOT NULL,
+  `ReportingContextId` INT NOT NULL,
+  `EffectiveStartTimestamp` VARCHAR(50) NULL,
+  `EffectiveEndTimestamp` VARCHAR(50) NULL,
+  `RatingAgencyID` VARCHAR(50) NULL,
+  `RatingTypeId` VARCHAR(50) NULL,
+  `RatingValue` VARCHAR(50) NULL,
+  `Ranking` VARCHAR(50) NULL,
+  `ECAICreditQualityStep` VARCHAR(50) NULL,
+  `IsInferredData` VARCHAR(50) NULL,
+  `ReferenceDataSnapshotId` VARCHAR(50) NULL,
+  `ProcessExecutionId` VARCHAR(50) NULL,
+  PRIMARY KEY (`RatingId`, `ReportingContextId`));
