eed8fc5e622ea7cf0e5ecd48514f0b149d52c70c
Vlad <yellow31031995@gmail.com>
2019-10-02 20:40:05 +0300

    Story 64: added to insert to data a table instrument

---
+++ Homework_5/SQL/Advanced tasks/2/Data population/change_to_insert_data_to_table_instrument.sql
@@ -0,0 +1,9 @@
+UPDATE `advanced_task2`.`instrument` SET `InstrumentId` = '1100', `ReportingContextId` = '111' WHERE (`InstrumentId` = '1110') and (`ReportingContextId` = '1');
+UPDATE `advanced_task2`.`instrument` SET `InstrumentId` = '1110', `ReportingContextId` = '112' WHERE (`InstrumentId` = '1120') and (`ReportingContextId` = '2');
+UPDATE `advanced_task2`.`instrument` SET `InstrumentId` = '1120', `ReportingContextId` = '113' WHERE (`InstrumentId` = '1130') and (`ReportingContextId` = '3');
+UPDATE `advanced_task2`.`instrument` SET `InstrumentId` = '1130', `ReportingContextId` = '114' WHERE (`InstrumentId` = '1140') and (`ReportingContextId` = '4');
+UPDATE `advanced_task2`.`instrument` SET `InstrumentId` = '1140', `ReportingContextId` = '115', `InstrumentCode` = '106662608' WHERE (`InstrumentId` = '1160') and (`ReportingContextId` = '6');
+UPDATE `advanced_task2`.`instrument` SET `InstrumentId` = '1150', `ReportingContextId` = '116', `InstrumentCode` = '107783527' WHERE (`InstrumentId` = '1170') and (`ReportingContextId` = '7');
+UPDATE `advanced_task2`.`instrument` SET `InstrumentId` = '1160', `ReportingContextId` = '117', `InstrumentCode` = '111674711' WHERE (`InstrumentId` = '1180') and (`ReportingContextId` = '8');
+UPDATE `advanced_task2`.`instrument` SET `InstrumentId` = '1170', `ReportingContextId` = '118' WHERE (`InstrumentId` = '1190') and (`ReportingContextId` = '9');
+INSERT INTO `advanced_task2`.`instrument` (`InstrumentId`, `ReportingContextId`, `InstrumentCode`) VALUES ('1180', '119', '114313533');
