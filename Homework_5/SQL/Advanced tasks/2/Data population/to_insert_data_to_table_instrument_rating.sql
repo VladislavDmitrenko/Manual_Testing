fc67a55bbceff3ac116f36c216b1e9131cf75886
Vlad <yellow31031995@gmail.com>
2019-10-02 19:39:55 +0300

    Story 57: to insert data a table instrument_rating

---
+++ Homework_5/SQL/Advanced tasks/2/Data population/to_insert_data_to_table_instrument_rating.sql
@@ -0,0 +1,9 @@
+INSERT INTO `advanced_task2`.`instument_rating` (`InstumentRatingId`, `ReportingContextId`, `InstrumentId`, `RatingId`) VALUES ('1', '111', '1110', '11101');
+INSERT INTO `advanced_task2`.`instument_rating` (`InstumentRatingId`, `ReportingContextId`, `InstrumentId`, `RatingId`) VALUES ('2', '112', '1120', '11201');
+INSERT INTO `advanced_task2`.`instument_rating` (`InstumentRatingId`, `ReportingContextId`, `InstrumentId`, `RatingId`) VALUES ('3', '113', '1130', '11301');
+INSERT INTO `advanced_task2`.`instument_rating` (`InstumentRatingId`, `ReportingContextId`, `InstrumentId`, `RatingId`) VALUES ('4', '114', '1140', '11401');
+INSERT INTO `advanced_task2`.`instument_rating` (`InstumentRatingId`, `ReportingContextId`, `InstrumentId`, `RatingId`) VALUES ('5', '115', '1150', '11501');
+INSERT INTO `advanced_task2`.`instument_rating` (`InstumentRatingId`, `ReportingContextId`, `InstrumentId`, `RatingId`) VALUES ('6', '116', '1160', '11601');
+INSERT INTO `advanced_task2`.`instument_rating` (`InstumentRatingId`, `ReportingContextId`, `InstrumentId`, `RatingId`) VALUES ('7', '117', '1170', '11701');
+INSERT INTO `advanced_task2`.`instument_rating` (`InstumentRatingId`, `ReportingContextId`, `InstrumentId`, `RatingId`) VALUES ('8', '118', '1180', '11801');
+INSERT INTO `advanced_task2`.`instument_rating` (`InstumentRatingId`, `ReportingContextId`, `InstrumentId`, `RatingId`) VALUES ('9', '119', '1190', '11901');
