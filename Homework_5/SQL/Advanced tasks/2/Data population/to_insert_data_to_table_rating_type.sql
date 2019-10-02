3a031e6d61880101ba8413d6625bcb02ddd4d82c
Vlad <yellow31031995@gmail.com>
2019-10-02 20:08:10 +0300

    Story 63: to insert to table rating_type

---
+++ Homework_5/SQL/Advanced tasks/2/Data population/to_insert_data_to_table_rating_type.sql
@@ -0,0 +1,9 @@
+INSERT INTO `advanced_task2`.`rating_type` (`RatingTypeId`, `ReportingContextId`, `RatingTypeCode`) VALUES ('11101', '111', 'LT');
+INSERT INTO `advanced_task2`.`rating_type` (`RatingTypeId`, `ReportingContextId`, `RatingTypeCode`) VALUES ('11201', '112', 'LT');
+INSERT INTO `advanced_task2`.`rating_type` (`RatingTypeId`, `ReportingContextId`, `RatingTypeCode`) VALUES ('11301', '113', 'LT');
+INSERT INTO `advanced_task2`.`rating_type` (`RatingTypeId`, `ReportingContextId`, `RatingTypeCode`) VALUES ('11401', '114', 'LT');
+INSERT INTO `advanced_task2`.`rating_type` (`RatingTypeId`, `ReportingContextId`, `RatingTypeCode`) VALUES ('11501', '115', 'LT');
+INSERT INTO `advanced_task2`.`rating_type` (`RatingTypeId`, `ReportingContextId`, `RatingTypeCode`) VALUES ('11601', '116', 'LT');
+INSERT INTO `advanced_task2`.`rating_type` (`RatingTypeId`, `ReportingContextId`, `RatingTypeCode`) VALUES ('11701', '117', 'RU');
+INSERT INTO `advanced_task2`.`rating_type` (`RatingTypeId`, `ReportingContextId`, `RatingTypeCode`) VALUES ('11801', '118', 'RU');
+INSERT INTO `advanced_task2`.`rating_type` (`RatingTypeId`, `ReportingContextId`, `RatingTypeCode`) VALUES ('11901', '119', 'RU');
