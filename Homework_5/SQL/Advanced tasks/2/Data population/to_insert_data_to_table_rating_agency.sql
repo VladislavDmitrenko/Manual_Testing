3f14f38d628985ae9d55aa479d3886829825b3d8
Vlad <yellow31031995@gmail.com>
2019-10-02 20:02:00 +0300

    Story 61: to insert to table rating_agency

---
+++ Homework_5/SQL/Advanced tasks/2/Data population/to_insert_data_to_table_rating_agency.sql
@@ -0,0 +1,9 @@
+INSERT INTO `advanced_task2`.`rating_agency` (`RatingAgencyID`, `ReportingContextId`, `RatingAgencyCode`, `RatingAgencyName`) VALUES ('101', '111', '10', 'Moody\\\'s');
+INSERT INTO `advanced_task2`.`rating_agency` (`RatingAgencyID`, `ReportingContextId`, `RatingAgencyCode`, `RatingAgencyName`) VALUES ('102', '112', '11', 'Moody\\\'s');
+INSERT INTO `advanced_task2`.`rating_agency` (`RatingAgencyID`, `ReportingContextId`, `RatingAgencyCode`, `RatingAgencyName`) VALUES ('103', '113', '12', 'Moody\\\'s');
+INSERT INTO `advanced_task2`.`rating_agency` (`RatingAgencyID`, `ReportingContextId`, `RatingAgencyCode`, `RatingAgencyName`) VALUES ('104', '114', '13', 'Moody\\\'s');
+INSERT INTO `advanced_task2`.`rating_agency` (`RatingAgencyID`, `ReportingContextId`, `RatingAgencyCode`, `RatingAgencyName`) VALUES ('105', '115', '14', 'Moody\\\'s');
+INSERT INTO `advanced_task2`.`rating_agency` (`RatingAgencyID`, `ReportingContextId`, `RatingAgencyCode`, `RatingAgencyName`) VALUES ('106', '116', '15', 'Barclays Internal');
+INSERT INTO `advanced_task2`.`rating_agency` (`RatingAgencyID`, `ReportingContextId`, `RatingAgencyCode`, `RatingAgencyName`) VALUES ('107', '117', '16', 'Standard & Poor\\\'s');
+INSERT INTO `advanced_task2`.`rating_agency` (`RatingAgencyID`, `ReportingContextId`, `RatingAgencyCode`, `RatingAgencyName`) VALUES ('108', '118', '17', 'Standard & Poor\\\'s');
+INSERT INTO `advanced_task2`.`rating_agency` (`RatingAgencyID`, `ReportingContextId`, `RatingAgencyCode`, `RatingAgencyName`) VALUES ('109', '119', '18', 'Standard & Poor\\\'s');
